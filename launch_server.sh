0957205612d62044998978ca20be08f0799286f1
David Mellons <dmellons@slusd.us>
2023-07-31 11:21:38 -0700

    Updated main.py form main2.py

---
+++ launch_server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+default="main"
+reload=""
+read -p "Run test or production? (1=production, 2=test): " version
+read -p "Run with reload flag? (y=reload, n=no): " q_reload
+server_file="$default"
+
+if [ "$version" == "2" ]; then
+    server_file="main_test"
+fi
+
+if [ "$q_reload" == "y" ]; then
+    reload="--reload"
+fi
+
+
+
+
+uvicorn "$server_file:app" " --host 0.0.0.0" " $reload"
